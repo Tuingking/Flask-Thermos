ff1d1c0689e9c0d3d6c8f6a33bace86e91187907
Aloysius Yoko <aloysius@xwork.id>
2017-08-12 09:57:55 +0700

    bookmark edit, flask-migrate

--- manage.py
+++ manage.py
@@ -5,10 +5,14 @@ This module depend on flask-script
 """
 
 from thermos import app, db
-from thermos.models import User
+from thermos.models import User, Bookmark
 from flask_script import Manager, prompt_bool
+from flask_migrate import Migrate, MigrateCommand
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def initdb():
@@ -23,7 +27,7 @@ def initdb():
 def dropdb():
     """Drop the database"""
     if prompt_bool(
-        "Are you sure you want to lose all your data"):
+            "Are you sure you want to lose all your data"):
         db.drop_all()
         print 'Dropped the database...'
 
--- thermos/models.py
+++ thermos/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import desc
+from sqlalchemy import desc  # descending order
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
